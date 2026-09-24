e07eae3a747fb6b0f6c5799d5bac346905e017d4
Mary Asiedu <ephratah1234@gmail.com>
2026-09-24 07:09:29 +0000

    Fix lint formatting

--- faker/providers/address/en_GH/__init__.py
+++ faker/providers/address/en_GH/__init__.py
@@ -1,5 +1,6 @@
 from .. import Provider as AddressProvider
 
+
 class Provider(AddressProvider):
     city_formats = ("{{city}}",)
     street_name_formats = ("{{street_name}}",)
@@ -7,9 +8,52 @@ class Provider(AddressProvider):
     address_formats = ("{{street_address}}\n{{city}}",)
     building_number_formats = ("##", "###", "###")
     street_names = (
-        "Oxford Street", "Liberation Road", "Independence Avenue", "Kwame Nkrumah Avenue", "George Bush Highway", "Cantonments Road", "Tema Motorway", "Ring Road", "Spintex Road", "Accra-Tema Highway", "Kumasi Road", "Airport Road",
+        "Oxford Street",
+        "Liberation Road",
+        "Independence Avenue",
+        "Kwame Nkrumah Avenue",
+        "George Bush Highway",
+        "Cantonments Road",
+        "Tema Motorway",
+        "Ring Road",
+        "Spintex Road",
+        "Accra-Tema Highway",
+        "Kumasi Road",
+        "Airport Road",
     )
     cities = (
-        "Accra", "Kumasi", "Tamale", "Takoradi", "Tema", "Cape Coast", "Sunyani", "Ho", "Bolgatanga", "Wa", "Koforidua", "Obuasi", "Sekondi", "Techiman", "Tarkwa", "Berekum", "Sefwi Wiawso", "Bibiani", "Mampong", "Axim", "Dunkwa-on-Offin", "Keta", "Salaga", "Bawku", "Navrongo", "Kpandu", "Hohoe", "Nalerigu", "Yendi", "Bimbilla", "Wenchi", "Dodi Papase", "Kintampo"
+        "Accra",
+        "Kumasi",
+        "Tamale",
+        "Takoradi",
+        "Tema",
+        "Cape Coast",
+        "Sunyani",
+        "Ho",
+        "Bolgatanga",
+        "Wa",
+        "Koforidua",
+        "Obuasi",
+        "Sekondi",
+        "Techiman",
+        "Tarkwa",
+        "Berekum",
+        "Sefwi Wiawso",
+        "Bibiani",
+        "Mampong",
+        "Axim",
+        "Dunkwa-on-Offin",
+        "Keta",
+        "Salaga",
+        "Bawku",
+        "Navrongo",
+        "Kpandu",
+        "Hohoe",
+        "Nalerigu",
+        "Yendi",
+        "Bimbilla",
+        "Wenchi",
+        "Dodi Papase",
+        "Kintampo",
     )
     countries = ("Ghana",)
--- faker/providers/person/en_GH/__init__.py
+++ faker/providers/person/en_GH/__init__.py
@@ -1,28 +1,58 @@
 from faker.providers.person import Provider as PersonProvider
 
+
 class Provider(PersonProvider):
     """
     Implement person provider for ` en_GH` locale.
     """
+
     first_names_male = (
-        "Kwame", "Kofi", "Yaw", "Fiifi", "Kojo", "Ekow", "Yoa", "Selasie",
+        "Kwame",
+        "Kofi",
+        "Yaw",
+        "Fiifi",
+        "Kojo",
+        "Ekow",
+        "Yoa",
+        "Selasie",
     )
 
     first_names_female = (
-        "Akosua", "Afia", "Abena", "Adwoa", "Ama", "Esi", "Yaa", "Akua", "Efua", "Aba",
-    )
-    
-    last_names = (
-        "Mensah", "Owusu", "Osei", "Boateng", "Agyeman", "Asante", "Acheampong",
-        "Appiah", "Boadu", "Amponsah", "Darko", "Yeboah", "Nkrumah", "Amoah", "Gyasi", "Frimpong", "Adu", "Quartey",
+        "Akosua",
+        "Afia",
+        "Abena",
+        "Adwoa",
+        "Ama",
+        "Esi",
+        "Yaa",
+        "Akua",
+        "Efua",
+        "Aba",
     )
 
-    formats_female = (
-        "{{first_name_female}} {{last_name}}",
+    last_names = (
+        "Mensah",
+        "Owusu",
+        "Osei",
+        "Boateng",
+        "Agyeman",
+        "Asante",
+        "Acheampong",
+        "Appiah",
+        "Boadu",
+        "Amponsah",
+        "Darko",
+        "Yeboah",
+        "Nkrumah",
+        "Amoah",
+        "Gyasi",
+        "Frimpong",
+        "Adu",
+        "Quartey",
     )
-     
-    formats_male = (
-        "{{first_name_male}} {{last_name}}",
-    )
+
+    formats_female = ("{{first_name_female}} {{last_name}}",)
+
+    formats_male = ("{{first_name_male}} {{last_name}}",)
 
     formats = formats_male + formats_female
